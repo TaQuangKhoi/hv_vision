a576c3a4418ed6cfc3a1ab8c7ece5004e152a194
Tạ Quang Khôi <khoi0941@gmail.com>
2023-11-06 19:01:48 +0700

    Add index view for convert_color

--- convert_color/views.py
+++ convert_color/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+
+# use render() instead of HttpResponse()
+def index_convert_color(request):
+    return render(request, template_name='index.html')
--- hv_vision/urls.py
+++ hv_vision/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # URL for Hảo Văn Apps
+    path('convert_color/', include('convert_color.urls')),
 ]
