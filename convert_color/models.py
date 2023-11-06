892d9bb6e5f59990fa34f806d8e55a66b12c9327
Tạ Quang Khôi <khoi0941@gmail.com>
2023-11-06 18:51:59 +0700

    Add model ImageProcessingResult
    - Install Pillow

--- convert_color/models.py
+++ convert_color/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class ImageProcessingResult(models.Model):
+    id = models.AutoField(primary_key=True)
+    image = models.ImageField(upload_to='images/')  # Store processed images
+
+    # Add fields to store information about the image processing
+    processing_date = models.DateTimeField(auto_now_add=True)
+    processing_method = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"Processed Image {self.id}"
