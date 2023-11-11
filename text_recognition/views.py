d93d03e0be5ad8f88c9a79df2fa9c1b62baea3e9
Tạ Quang Khôi <khoi0941@gmail.com>
2023-11-11 22:09:46 +0700

    Save views

--- text_recognition/views.py
+++ text_recognition/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(
+        request,
+        template_name='index.jinja',
+        context={
+            'image': 'https://i.pinimg.com/736x/20/c9/9c/20c99c680ffe0b40127af797a535a225.jpg',
+        }
+    )
