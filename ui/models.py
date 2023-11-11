fdae67d6c3ea1efc1bb4242f5533963eaba356e6
Tạ Quang Khôi <khoi0941@gmail.com>
2023-11-11 22:06:53 +0700

    Build model for app

--- ui/models.py
+++ ui/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+
 # Create your models here.
+class Requests(models.Model):
+    id = models.AutoField(primary_key=True)
+    server = models.OneToOneField('Servers', on_delete=models.CASCADE)
+    current_rank = models.OneToOneField('Ranks', on_delete=models.CASCADE)
+    desired_rank = models.OneToOneField('Ranks', on_delete=models.CASCADE)
+    champion = models.OneToOneField('Champions', on_delete=models.CASCADE)
+
+
+class Ranks(models.Model):
+    id = models.AutoField(primary_key=True)
+
+
+class Champions(models.Model):
+    id = models.AutoField(primary_key=True)
+
+
+class Servers(models.Model):
+    id = models.AutoField(primary_key=True)
+
+
+class GameTypes(models.Model):
+    id = models.AutoField(primary_key=True)
