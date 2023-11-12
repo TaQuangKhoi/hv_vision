23b018df9dd369c1dd1647e2bda80559d92c7238
Tạ Quang Khôi <khoi0941@gmail.com>
2023-11-12 10:13:28 +0700

    Update code css and js

---
+++ text_recognition/static/tr/js/script.js
@@ -0,0 +1,9 @@
+
+function clickImageBox() {
+    console.debug("Khôi yêu Hảo")
+    let imageBox = document.getElementById("image-upload")
+    imageBox.click()
+
+    let formUpload = document.getElementById("form-upload")
+    console.debug(formUpload)
+}
