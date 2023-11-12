bf78a89736a79724c03a39897a4aa1e011c9a636
Tạ Quang Khôi <khoi0941@gmail.com>
2023-11-12 11:23:03 +0700

    Change image preview by upload file

--- text_recognition/static/tr/js/script.js
+++ text_recognition/static/tr/js/script.js
@@ -1,8 +1,18 @@
 
 function clickImageBox() {
     console.debug("Khôi yêu Hảo")
-    let imageBox = document.getElementById("image-upload")
-    imageBox.click()
+    let imageInput = document.getElementById("image-upload")
+    imageInput.click()
+    // get image from click event
+    imageInput.addEventListener("change", function () {
+        let image = imageInput.files[0]
+        let reader = new FileReader()
+        reader.onload = function (e) {
+            let imagePreview = document.getElementById("image-preview")
+            imagePreview.src = e.target.result
+        }
+        reader.readAsDataURL(image)
+    });
 
     let formUpload = document.getElementById("form-upload")
     console.debug(formUpload)
