b6cd9acef141533af974f5f07ff32ab0a1af396c
Tạ Quang Khôi <khoi0941@gmail.com>
2023-11-12 15:24:13 +0700

    Install opencv-python

--- text_recognition/views.py
+++ text_recognition/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
-
+import cv2
 
 def index(request):
+    """Index page view"""
+
+    if request.method == 'POST':
+        # get values from form
+        data = request.POST
+        name = data.get("name")
+        print(name)
+
+        files = request.FILES
+        image_upload = files.get("image-upload")
+        print(type(image_upload))
+        # save image to media folder
+        # with open(f"media/test.png", 'wb+') as destination:
+        #     for chunk in image_upload.chunks():
+        #         destination.write(chunk)
+
     return render(
         request,
         template_name='tr_index.jinja',
