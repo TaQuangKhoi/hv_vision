23848b527552726109fb0d05f0f8efce25c6ea5d
Tạ Quang Khôi <khoi0941@gmail.com>
2023-11-13 13:10:16 +0700

    Use data in memory to process

--- text_recognition/utils.py
+++ text_recognition/utils.py
@@ -6,24 +6,28 @@ import base64
 def save_canny_image(img):
     file_dir = "text_recognition/static/results/"
 
-    cv2.imdecode(np.fromstring(img.read(), np.uint8), cv2.IMREAD_UNCHANGED)
-
-    file_name = file_dir + "test.png"
-    # Save the image
-    with open(file_name, 'wb+') as destination:
-        for chunk in img.chunks():
-            destination.write(chunk)
+    # file_name = file_dir + "test.png"
+    # # Save the image
+    # with open(file_name, 'wb+') as destination:
+    #     for chunk in img.chunks():
+    #         destination.write(chunk)
 
     # Open the image in OpenCV
-    img = cv2.imread(file_name)
+    img_cv2 = cv2.imdecode(
+        np.fromstring(img.read(), np.uint8),
+        cv2.IMREAD_UNCHANGED
+    )
+    origin_content = cv2.imencode('.jpg', img_cv2)[1].tostring()
+    encoded_origin = base64.encodebytes(origin_content)
+    origin = 'data:image/jpg;base64, ' + str(encoded_origin, 'utf-8')
 
     # Apply Canny
-    edges = cv2.Canny(img, 100, 200, 3, L2gradient=True)
+    edges = cv2.Canny(img_cv2, 100, 200, 3, L2gradient=True)
+    image_result_content = cv2.imencode('.jpg', edges)[1].tostring()
+    encoded_result = base64.encodebytes(image_result_content)
 
-    image_content = cv2.imencode('.jpg', edges)[1].tostring()
-    encoded_image = base64.encodebytes(image_content)
-    result = 'data:image/jpg;base64, ' + str(encoded_image, 'utf-8')
+    result = 'data:image/jpg;base64, ' + str(encoded_result, 'utf-8')
 
     # cv2.imwrite(file_dir + "test_canny.png", edges)
 
-    return ["/static/results/" + "test.png", result]
+    return [origin, result]
