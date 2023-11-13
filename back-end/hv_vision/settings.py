d7aa334ad2f6a6fb7fc81e9f1d52c63e37e17382
Tạ Quang Khôi <khoi0941@gmail.com>
2023-11-14 00:23:36 +0700

    Update backend database

--- back-end/hv_vision/settings.py
+++ back-end/hv_vision/settings.py
@@ -84,8 +84,15 @@ WSGI_APPLICATION = 'hv_vision.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql',
+        "NAME": os.environ.get('DB_NAME'),
+        "USER": os.environ.get('DB_USER'),
+        "PASSWORD": os.environ.get('DB_USER_PASSWORD'),
+        "HOST": os.environ.get('DB_HOST'),
+        "PORT": os.environ.get('DB_PORT'),
     }
 }
 
