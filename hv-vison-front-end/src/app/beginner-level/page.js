45856e429190f6df7737ddee51f043baf2cba34c
Tạ Quang Khôi <khoi0941@gmail.com>
2023-11-14 12:48:56 +0700

    Add Loading indicator for button

--- hv-vison-front-end/src/app/beginner-level/page.js
+++ hv-vison-front-end/src/app/beginner-level/page.js
@@ -7,6 +7,8 @@ import {data} from "autoprefixer";
 export default function BeginnerPage() {
     const [imageUploadSrc, setImageUploadSrc] = useState('/when-no-image.png');
     const [imagePreviewSrc, setImagePreviewSrc] = useState('/when-no-image.png');
+    const [cannyLoading, setCannyLoading] = useState(false);
+    const [contoursLoading, setContoursLoading] = useState(false);
 
     let endpoint = process.env.NEXT_PUBLIC_DJANGO_API_DOMAIN ? process.env.NEXT_PUBLIC_DJANGO_API_DOMAIN : 'http://127.0.0.1:8000'
 
@@ -25,6 +27,7 @@ export default function BeginnerPage() {
         let data = new FormData();
         data.append('file', input.files[0]);
 
+        setCannyLoading(true)
         await fetch(endpoint + '/canny/create/', {
             method: 'POST',
             body: data
@@ -34,6 +37,7 @@ export default function BeginnerPage() {
             let json = JSON.parse(data)
             // setImageUploadSrc(json.imageUpload)
             setImagePreviewSrc(json.imagePreview)
+            setCannyLoading(false)
         }).catch(error => {
             console.error(error)
         })
@@ -44,6 +48,7 @@ export default function BeginnerPage() {
         let data = new FormData();
         data.append('file', input.files[0]);
 
+        setContoursLoading(true)
         await fetch(endpoint + '/contours/create/', {
             method: 'POST',
             body: data
@@ -53,6 +58,7 @@ export default function BeginnerPage() {
             let json = JSON.parse(data)
             // setImageUploadSrc(json.imageUpload)
             setImagePreviewSrc(json.imagePreview)
+            setContoursLoading(false)
         }).catch(error => {
             console.error(error)
         })
@@ -86,15 +92,34 @@ export default function BeginnerPage() {
                 <ul>
                     <li>
                         <button onClick={canny}
-                                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-                                type="submit" id="canny-submit-button">
+                                className="inline-flex items-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                                type="button" id="canny-submit-button">
+                            {cannyLoading ?
+                                <svg className="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24"
+                                     aria-hidden={cannyLoading}>
+                                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
+                                            stroke-width="4"></circle>
+                                    <path className="opacity-75" fill="currentColor"
+                                          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                                </svg>
+                                : ''}
+
                             Canny
                         </button>
                     </li>
                     <li>
                         <button onClick={contours}
-                                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-                                type="submit" id="contours-submit-button">
+                                className="inline-flex items-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                                type="button" id="contours-submit-button">
+                            {contoursLoading ?
+                                <svg className="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24"
+                                     aria-hidden={cannyLoading}>
+                                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
+                                            stroke-width="4"></circle>
+                                    <path className="opacity-75" fill="currentColor"
+                                          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                                </svg>
+                                : ''}
                             Contours
                         </button>
                     </li>
