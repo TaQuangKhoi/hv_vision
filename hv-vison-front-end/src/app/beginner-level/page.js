e97d4bb370454bec52aabc4162ddb606068eb3b5
Tạ Quang Khôi <khoi0941@gmail.com>
2023-11-14 19:49:48 +0700

    Make Responsive design

--- hv-vison-front-end/src/app/beginner-level/page.js
+++ hv-vison-front-end/src/app/beginner-level/page.js
@@ -84,9 +84,10 @@ export default function BeginnerPage() {
 
     return (
         <main>
-            <div className="flex justify-center items-center text-center min-h-[100vh]">
-                <div onClick={uploadImage}
-                     className="h-[500px] w-[500px] m-auto rounded-[10px] border-[#0a53be] border-2 hover:cursor-pointer">
+            <button title="Contact Sale"
+                    className="fixed z-90 top-10 left-8 bg-blue-600 w-20 h-20 rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl hover:bg-blue-700 hover:drop-shadow-2xl hover:animate-bounce duration-300">&#9993;</button>
+            <div className="flex sm:flex-col md:flex-col lg:flex-row  justify-center items-center text-center min-h-[100vh]">
+                <div className="h-[500px] w-[500px] m-auto rounded-[10px] border-[#0a53be] border-2 hover:cursor-pointer" onClick={uploadImage}>
                     <Image width={498} height={498} className={'h-[498px] w-[498px] !object-contain rounded-[10px]'}
                            src={imageUploadSrc} id="image-upload" alt="Ảnh Hảo Upload"/>
                 </div>
@@ -99,7 +100,7 @@ export default function BeginnerPage() {
                                 <svg className="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24"
                                      aria-hidden={cannyLoading}>
                                     <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
-                                            stroke-width="4"></circle>
+                                            strokeWidth="4"></circle>
                                     <path className="opacity-75" fill="currentColor"
                                           d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                                 </svg>
