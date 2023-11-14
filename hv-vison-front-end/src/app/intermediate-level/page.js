e3f8283c5812b9b27fe1e3aba0b6843f84a5d982
Tạ Quang Khôi <khoi0941@gmail.com>
2023-11-14 20:21:22 +0700

    Update page intermediate

--- hv-vison-front-end/src/app/intermediate-level/page.js
+++ hv-vison-front-end/src/app/intermediate-level/page.js
@@ -3,34 +3,11 @@ import Image from "next/image";
 export default function BeginnerPage() {
     return (
         <main>
-            <div className="center-middle-screen">
-                <div className="image-box" onClick="clickImageBox()">
-                    <Image src="" id="image-upload" alt="Ảnh Hảo"/>
-                </div>
-                <ul>
-                    <li>
-                        <button className="btn btn-primary" type="submit" id="canny-submit-button">
-                            Canny
-                        </button>
-                    </li>
-                    <li>
-                        <button className="btn btn-primary" type="submit" id="contours-submit-button">
-                            Contours
-                        </button>
-                    </li>
-                </ul>
-                <div className="image-box">
-                    <Image src="" id="image-preview" alt="Ảnh Hảo"/>
-                </div>
+            <div className="flex justify-center items-center text-center text-2xl min-h-[100vh]">
+                <p>
+                    Coming soon...
+                </p>
             </div>
-            <form action="" method="post" encType="multipart/form-data" id="form-file">
-                <input hidden="hidden"
-                       type="file"
-                       name="imageInput"
-                       id="imageInput"
-                       accept="image/png, image/jpeg, image/jpg"
-                />
-            </form>
         </main>
     )
 }
