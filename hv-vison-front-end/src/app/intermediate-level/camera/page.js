fee0dd9ba0f1063e9ba4afc7a49c6bf707efac3a
Tạ Quang Khôi <khoi0941@gmail.com>
2023-11-15 18:25:43 +0700

    Use Link to go to home

--- hv-vison-front-end/src/app/intermediate-level/camera/page.js
+++ hv-vison-front-end/src/app/intermediate-level/camera/page.js
@@ -2,6 +2,7 @@
 
 import {useState} from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function CameraPage() {
     const [videoSrc, setVideoSrc] = useState(null);
@@ -110,17 +111,15 @@ export default function CameraPage() {
     let endpoint = process.env.NEXT_PUBLIC_DJANGO_API_DOMAIN ? process.env.NEXT_PUBLIC_DJANGO_API_DOMAIN : 'http://127.0.0.1:8000'
 
     return <>
-        <button onClick={test}
+        <Link href={"/"}
                 className="inline-flex items-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
                 type="button" id="canny-submit-button">
             Canny
-        </button>
-        <div
-            className="w-[960px] h-[540px] m-auto rounded-[10px] border-[#0a53be] border-2">
+        </Link>
+        <div className="w-[960px] h-[540px] m-auto rounded-[10px] border-[#0a53be] border-2">
             <img className="w-[960px] h-[540px] !object-contain rounded-[10px]" id="image"
                  src={endpoint + '/camera/video/'} alt="Ảnh Hảo Upload"
             />
         </div>
-
     </>
 }
