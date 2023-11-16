a1cd720fd99e9fcea07038daffc9b8b263835166
Tạ Quang Khôi <khoi0941@gmail.com>
2023-11-16 07:52:57 +0700

    Add UI to update Rtmp Url

--- hv-vison-front-end/src/app/intermediate-level/camera/page.js
+++ hv-vison-front-end/src/app/intermediate-level/camera/page.js
@@ -32,6 +32,33 @@ export default function CameraPage() {
         }
     }, []);
 
+    function updateRtmpUrl() {
+        let rtmpUrl = document.getElementById('rtmp_url').value
+        if (rtmpUrl === '') {
+            toast.error('Please enter rtmp url')
+            return
+        }
+        fetch(apiEndpoint + '/camera/rtmp_url/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                rtmp_url: rtmpUrl
+            })
+        }).then(response => {
+            let status = response.status
+            if (status === 200) {
+                toast.success('Update rtmp url successfully')
+            } else {
+                toast.error('Update rtmp url failed')
+            }
+        }).catch(error => {
+            toast.error('Update rtmp url failed')
+        })
+    }
+
+
     return <>
         <button onClick={
             () => {
@@ -64,6 +91,16 @@ export default function CameraPage() {
                 type="button" id="canny-submit-button">
             Faces detect
         </button>
+
+        <div className="grid gap-6 mb-6 grid-cols-2">
+            <input type="text" id="rtmp_url" name="rtmp_url"
+                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                   placeholder="rtmp://35.185.190.46/live/keios" required/>
+            <button type="submit" onClick={updateRtmpUrl}
+                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Update
+            </button>
+        </div>
 
         <div className="w-[960px] h-[540px] m-auto rounded-[10px] border-[#0a53be] border-2">
             <CvImage src={imageSrc}/>
