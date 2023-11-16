1d88becbb72c0535c6f40be4fb80b1e4f61893e2
Tạ Quang Khôi <khoi0941@gmail.com>
2023-11-16 08:28:12 +0700

    Update UI, add button back

--- hv-vison-front-end/src/app/intermediate-level/camera/page.js
+++ hv-vison-front-end/src/app/intermediate-level/camera/page.js
@@ -3,6 +3,7 @@
 import {useEffect, useState} from "react";
 import {toast} from "react-toastify";
 import apiEndpoint from "@/django_api_endpoint";
+import Link from "next/link";
 
 function CvImage({src}) {
     return <img src={src}
@@ -59,7 +60,23 @@ export default function CameraPage() {
     }
 
 
-    return <>
+    return <main className="p-3">
+        <Link href={'/intermediate-level'}
+              className="mb-4 w-20 h-20 rounded-full flex justify-center items-center text-white text-4xl hover:bg-blue-700 hover:drop-shadow-2xl hover:animate-bounce duration-300"
+        >
+            <img src="https://cdn-icons-png.flaticon.com/512/7915/7915208.png"/>
+        </Link>
+
+        <div className="grid gap-6 mb-6 grid-cols-2">
+            <input type="text" id="rtmp_url" name="rtmp_url"
+                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                   placeholder="rtmp://35.185.190.46/live/keios" required/>
+            <button type="submit" onClick={updateRtmpUrl}
+                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Update
+            </button>
+        </div>
+
         <button onClick={
             () => {
                 setImageSrc('/when-no-image.png')
@@ -92,18 +109,8 @@ export default function CameraPage() {
             Faces detect
         </button>
 
-        <div className="grid gap-6 mb-6 grid-cols-2">
-            <input type="text" id="rtmp_url" name="rtmp_url"
-                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                   placeholder="rtmp://35.185.190.46/live/keios" required/>
-            <button type="submit" onClick={updateRtmpUrl}
-                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                Update
-            </button>
-        </div>
-
         <div className="w-[960px] h-[540px] m-auto rounded-[10px] border-[#0a53be] border-2">
             <CvImage src={imageSrc}/>
         </div>
-    </>
+    </main>
 }
